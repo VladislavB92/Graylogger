eca602b0e37e307bc715cf255a06991ce9ed7a03
v.ofitserov <v.ofitserov@systtech.ru>
2023-03-18 17:23:42 +0200

    add info to init file

--- graylogging/__init__.py
+++ graylogging/__init__.py
@@ -1,1 +1,11 @@
-from .handlers.HTTPHandler import GraylogHTTPHandler
+"""
+:authors: v.oficerov
+:license: MIT License
+:copyright: (c) 2023 v.oficerov
+"""
+
+from .handlers.HTTPHandler import HTTPHandler
+
+__author__ = 'v.oficerov'
+__version__ = '0.1.0'
+__email__ = 'valeryoficerov@gmail.com'
